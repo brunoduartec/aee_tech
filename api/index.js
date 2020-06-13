8e8915b6a01b71045c35643e9c4797dad91839e3
brunoduartec <bruno.duartec@gmail.com>
2020-06-13 15:52:31 -0300

    configure api

--- api/index.js
+++ api/index.js
@@ -1,11 +1,11 @@
 const express = require('express')
-const mongoose = require('mongoose');
-const user = require('./routes/user');
 const bodyParser = require('body-parser');
+const Database = require('./helper/Database')
+const env = require('./env.json')
 
+const database = new Database(env.mysql).getInstance();
 
-const PORT = 3000;
-const HOST = '0.0.0.0';
+const usuario = require('./routes/usuario');
 
 const app = express();
 
@@ -15,20 +15,8 @@ app.get('/', function (req, res) {
     res.send("Hello World doidão");
 });
 
+app.use('/api/usuario', usuario);
 
-
-app.use('/api/user', user);
-
-mongoose
-    .connect('mongodb://db:27017/crud-node-mongo-docker', {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-    })
-    .then(result => {
-        console.log('MongoDB Conectado');
-    })
-    .catch(error => {
-        console.log(error);
-    });
-
-app.listen(PORT, HOST)
+app.listen(env.port, env.host, () => {
+    console.log("Listening at port:" + env.port)
+})
