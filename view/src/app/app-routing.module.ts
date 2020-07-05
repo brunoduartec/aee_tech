0c1e567a81c8e5cc43b26272f82f44ddd94fd55d
Pedro Henrique Dornellas Faria <faria.pedrohd@gmail.com>
2020-07-05 13:35:48 -0300

    Inicio Front End

--- view/src/app/app-routing.module.ts
+++ view/src/app/app-routing.module.ts
@@ -2,10 +2,15 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { CentroListComponent } from './centro-list/centro-list.component';
 
+const centro ="centros";
 
 const routes: Routes = [{
-  path: "centros",
+  path: centro,
   component: CentroListComponent
+},
+{
+  path: '**',
+  redirectTo: centro
 }];
 
 @NgModule({
