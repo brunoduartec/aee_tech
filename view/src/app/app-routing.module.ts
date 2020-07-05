ceda0da0af2d0514882f08f34f68e5f2e77953b4
brunoduartec <bruno.duartec@gmail.com>
2020-07-05 12:12:18 -0300

    add centro routing

--- view/src/app/app-routing.module.ts
+++ view/src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CentroListComponent } from './centro-list/centro-list.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: "centros",
+  component: CentroListComponent
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
