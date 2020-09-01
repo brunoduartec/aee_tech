c862a28ecf0fdd93b15cb2176acd0528c141b10b
Bruno Duarte Corrêa <bruno.duartec@gmail.com>
2020-09-01 20:46:25 -0300

    fix ipv6 issue

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ function createMasterNode(cluster) {
 
 function createSlaveNode() {
     let server_http = http.Server(api);
-    server_http.listen(port, "127.0.0.1", function () {
+    server_http.listen(port, function () {
         console.log("API is running on port: " + port)
     })
 
